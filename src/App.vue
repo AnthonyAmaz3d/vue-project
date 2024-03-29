260228734574ac35adeb164f350729bdbbe52cae
AnthonyAmaz3d <anthonymaia00@gmail.com>
2024-03-28 21:25:35 -0300

    finalizando o projeto

--- src/App.vue
+++ src/App.vue
@@ -3,63 +3,65 @@ import { reactive } from 'vue';
 
 const estado = reactive({
   resultado: 0,
-  filter: "adicao",
 })
 
+let numero1 = 0
+let numero2 = 0
+let filter = "0"
+
 const adicionaNumero1 = (evento) => {
-  const numero1 = evento.target.value;
-  console.log(numero1)
+  numero1 = parseInt(evento.target.value);
+}
+
+const valorSelect = (evento) => {
+  filter = evento.target.value;
 }
 
 const adicionaNumero2 = (evento) => {
-  const numero2 = evento.target.value
-  console.log(numero2)
+  numero2 = parseInt(evento.target.value);
 }
 
-const trocarOperacao = () => {
-  const { filter } = estado;
-    switch(filter) {
-      case 'subtracao':
-        return diminuir();
-      case 'multiplicacao':
-        return multiplicar();
-      case 'divisao':
-        return dividir();
-      default:
-        return somar();
-    }
+const trocaFiltro = () => {
+  switch (filter) {
+  case "adicao":
+    return somar(numero1, numero2);
+  case "subtracao":
+    return diminuir(numero1, numero2);
+  case "multiplicacao":
+    return multiplicar(numero1, numero2);
+  case "divisao":
+    return dividir(numero1, numero2);
+}}
+
+const somar = (numero1, numero2) => {
+  return estado.resultado = numero1 + numero2;
 }
 
-const somar = () => {
-  estado.resultado = numero1 + numero2;
+const diminuir = (numero1, numero2) => {
+  return estado.resultado = numero1 - numero2;
 }
 
-const diminuir = () => {
-  estado.resultado = numero1 - numero2;
+const multiplicar = (numero1, numero2) => {
+  return estado.resultado = numero1 * numero2;
 }
 
-const multiplicar = () => {
-  estado.resultado = numero1 * numero2;
+const dividir = (numero1, numero2) => {
+  return estado.resultado = numero1 / numero2;
 }
-
-const dividir = () => {
-  estado.resultado = numero1 / numero2;
-}
-
 </script>
 
 <template>
-  <div class="container mb-4 mt-4 bg-secondary pt-2 ">
+  <div class="container mb-4 mt-4 bg-secondary pt-2 rounded-3">
     <header class="mb-4">
       <h1>Calculador em Vue</h1>
     </header>
-
     <div class="row">
       <div class="col-3">
         <input class="form-control" type="number" placeholder="Digite o primeiro número" @keyup="adicionaNumero1">
       </div>
       <div class="col-2">
-        <select class="form-select" @change="trocarOperacao()">
+        <select @change="valorSelect" class="form-select">
+          <option value="0"></option>
           <option value="adicao">Adição(+)</option>
           <option value="subtracao">Subtração(-)</option>
           <option value="multiplicacao">Multiplicação(*)</option>
@@ -70,7 +72,7 @@ const dividir = () => {
         <input class="form-control" type="number" placeholder="Digite o segundo número" @keyup="adicionaNumero2">
       </div>
       <div class="col">
-        <button class="btn btn-dark">Calcular</button>
+        <button @click="trocaFiltro" class="btn btn-dark">Calcular</button>
       </div>
       <div class="col">
         <p class="form-control" placeholder="Resultado:">
